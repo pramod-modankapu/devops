994044eb4a3f03072a4c885708f15d208f05707d
pramod-modankapu <51256082+pramod-modankapu@users.noreply.github.com>
2019-06-07 12:42:52 +0000

    updating

--- src/main/java/com/scmgalaxy/mavensample/App.java
+++ src/main/java/com/scmgalaxy/mavensample/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Hello World RefX! MAH-2--d");
+        log.debug("Hello World RefX! MAH-2--d");
     }
 }
